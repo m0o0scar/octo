036697e0985252402a858653237da00418cf612e
Oscar <165961+m0o0scar@users.noreply.github.com>
2023-03-07 12:00:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<img src="public/icon-128.png" alt="logo"/>
+
+# Chrome Extension Boilerplate
+
+React + Vite + TypeScript + TailwindCSS + DaisyUI
---
+++ src/pages/options/Options.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Options: React.FC = () => {
+  return <div className="OptionsContainer">Options</div>;
+};
+
+export default Options;
